53c4372f4bceabafea8fcc35e65d4e5a7818a65b
Mukesh Rajbanshi <59588637+mukesh-890@users.noreply.github.com>
2022-05-21 14:34:25 +0530

    initial commit

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,15 @@
 // import using require
 
 // declare class
+class Circle extends drawShape {
+	 
+    calculateArea(radius) {
+		console.log("Area is" + 3.14 * radius * radius);
+	}
+    color() {
+		console.log("Red");
+	}
 
+}
 
 // export class using module.exports
